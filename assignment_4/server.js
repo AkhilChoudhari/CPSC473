d83465da2df6c9508d1e916ce16984e7676c5fd7
nikuldchauhan <dhaval301092@gmail.com>
2015-03-15 23:51:45 -0700

    Working application only for rock

--- assignment_4/server.js
+++ assignment_4/server.js
@@ -1,6 +1,10 @@
 
 var http = require("http"),
+    querystring = require("querystring"),
     server;
+    var resultArray = '{"outcome":"John","wins":0,"losses":0,"ties":0}';
+    var gameArray = ["rock", "paper", "scissors", "lizard", "spock"];
+    var resultJSON=JSON.parse(resultArray);
     function beginPage(res) {
     res.write("<!DOCTYPE html>\n");
     res.write("<html lang='en'>\n");
@@ -21,29 +25,76 @@ var http = require("http"),
     function postAction(res,action)
     {
       res.write("<form method='POST' action='/play/"+action+"'>\n");
+      res.write("<input type='hidden' name='element' value='"+action+"'>\n");
       res.write("<input type='submit' value='"+action+"'>\n");
       res.write("</form><br/>");
     }
 
-    function frontPage(req, res) {
-        res.writeHead(200, {
-            "Content-Type": "text/html"
-        });
-        beginPage(res);
-        //res.write(req.method +"" +req.url);
-        if (req.method === "POST" && (req.url === "/play/rock" || req.url === "/play/paper" ||
-        req.url === "/play/scissor"  || req.url === "/play/lizard" || req.url === "/play/spock")) {
-          res.write(req.url);
+    function gameLogic(res,requestEle,resultJSON,gameArray)
+    {
+      var selectEle=gameArray[Math.floor(Math.random() * gameArray.length)];
+      if(requestEle==="rock")
+      {
+        if(selectEle ==="scissors" || selectEle ==="lizard")
+        {
+          resultJSON.outcome="win";
+          resultJSON.wins += 1;
+        }
+        else if(selectEle === "rock")
+        {
+          resultJSON.outcome="ties";
+          resultJSON.ties += 1;
+
         }
         else
         {
+          resultJSON.outcome="losses";
+          resultJSON.losses += 1;
+        }
+      }
+      return resultJSON;
+    }
+
+    function frontPage(req, res) {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+
+        var requestEle;
+        if (req.method === "POST" && (req.url === "/play/rock" || req.url === "/play/paper" ||
+        req.url === "/play/scissor"  || req.url === "/play/lizard" || req.url === "/play/spock")) {
+
+              /*var chunk = '';
+              req.on('data', function (data) {
+                  chunk += data;
+              });
+              req.on('end', function () {
+                requestEle=(querystring.parse(chunk)).element;
+                console.log("request"+requestEle);
+              });*/
+              var parts=req.url.split("/"),
+              requestEle=parts[2];
+              resultJSON=gameLogic(res,requestEle,resultJSON,gameArray)
+              res.write(JSON.stringify(resultJSON));
+              res.end();
+        }
+        else
+        {
+          res.writeHead(200, {
+              "Content-Type": "text/html"
+          });
+          beginPage(res);
+
           postAction(res,"rock");
           postAction(res,"paper");
           postAction(res,"scissor");
           postAction(res,"lizard");
           postAction(res,"spock");
+
+          endPage(res);
         }
-        endPage(res);
+
 
     }
 server=http.createServer(frontPage);
