b76facd81684ba24e68385dc29da293e781a8171
nikuldchauhan <dhaval301092@gmail.com>
2015-03-16 11:20:22 -0700

    Beautify and Jshint checked

--- assignment_4/server.js
+++ assignment_4/server.js
@@ -1,11 +1,10 @@
-
 var http = require("http"),
-    querystring = require("querystring"),
     server;
-    var resultArray = '{"outcome":"John","wins":0,"losses":0,"ties":0}';
-    var gameArray = ["rock", "paper", "scissors", "lizard", "spock"];
-    var resultJSON=JSON.parse(resultArray);
-    function beginPage(res) {
+var resultArray = '{"outcome":"John","wins":0,"losses":0,"ties":0}';
+var gameArray = ["rock", "paper", "scissors", "lizard", "spock"];
+var resultJSON = JSON.parse(resultArray);
+
+function beginPage(res) {
     res.write("<!DOCTYPE html>\n");
     res.write("<html lang='en'>\n");
     res.write("<head>\n");
@@ -14,171 +13,133 @@ var http = require("http"),
     res.write("</head>\n");
     res.write("<body>\n");
     res.write("<div class='container'>");
- }
+}
 
- function endPage(res) {
-     res.write("</div>");
-     res.write("</body>\n");
-     res.write("</html>\n");
-     res.end();
- }
-    function postAction(res,action)
-    {
-      res.write("<form method='POST' action='/play/"+action+"'>\n");
-      res.write("<input type='hidden' name='element' value='"+action+"'>\n");
-      res.write("<input type='submit' value='"+action+"'>\n");
-      res.write("</form><br/>");
+function endPage(res) {
+    res.write("</div>");
+    res.write("</body>\n");
+    res.write("</html>\n");
+    res.end();
+}
+
+function postAction(res, action) {
+    res.write("<form method='POST' action='/play/" + action + "'>\n");
+    res.write("<input type='hidden' name='element' value='" + action + "'>\n");
+    res.write("<input type='submit' value='" + action + "'>\n");
+    res.write("</form><br/>");
+}
+
+function gameLogic(requestEle, resultJSON, gameArray) {
+    var selectEle = gameArray[Math.floor(Math.random() * gameArray.length)];
+    console.log(selectEle);
+    if (requestEle === "rock") {
+        if (selectEle === "scissors" || selectEle === "lizard") {
+            resultJSON.outcome = "win";
+            resultJSON.wins += 1;
+        } else if (selectEle === "rock") {
+            resultJSON.outcome = "ties";
+            resultJSON.ties += 1;
+
+        } else {
+            resultJSON.outcome = "losses";
+            resultJSON.losses += 1;
+        }
+    } else if (requestEle === "paper") {
+        if (selectEle === "rock" || selectEle === "spock") {
+            resultJSON.outcome = "win";
+            resultJSON.wins += 1;
+        } else if (selectEle === "paper") {
+            resultJSON.outcome = "ties";
+            resultJSON.ties += 1;
+
+        } else {
+            resultJSON.outcome = "losses";
+            resultJSON.losses += 1;
+        }
+    } else if (requestEle === "scissors") {
+        if (selectEle === "paper" || selectEle === "lizard") {
+            resultJSON.outcome = "win";
+            resultJSON.wins += 1;
+        } else if (selectEle === "scissors") {
+            resultJSON.outcome = "ties";
+            resultJSON.ties += 1;
+
+        } else {
+            resultJSON.outcome = "losses";
+            resultJSON.losses += 1;
+        }
+    } else if (requestEle === "lizard") {
+        if (selectEle === "paper" || selectEle === "spock") {
+            resultJSON.outcome = "win";
+            resultJSON.wins += 1;
+        } else if (selectEle === "lizard") {
+            resultJSON.outcome = "ties";
+            resultJSON.ties += 1;
+
+        } else {
+            resultJSON.outcome = "losses";
+            resultJSON.losses += 1;
+        }
+    } else if (requestEle === "spock") {
+        if (selectEle === "scissors" || selectEle === "rock") {
+            resultJSON.outcome = "win";
+            resultJSON.wins += 1;
+        } else if (selectEle === "spock") {
+            resultJSON.outcome = "ties";
+            resultJSON.ties += 1;
+
+        } else {
+            resultJSON.outcome = "losses";
+            resultJSON.losses += 1;
+        }
+    } else {
+        resultJSON = {};
     }
+    return resultJSON;
+}
 
-    function gameLogic(res,requestEle,resultJSON,gameArray)
-    {
-      var selectEle=gameArray[Math.floor(Math.random() * gameArray.length)];
-      console.log(selectEle);
-      if(requestEle==="rock")
-      {
-        if(selectEle ==="scissors" || selectEle ==="lizard")
-        {
-          resultJSON.outcome="win";
-          resultJSON.wins += 1;
-        }
-        else if(selectEle === "rock")
-        {
-          resultJSON.outcome="ties";
-          resultJSON.ties += 1;
+function frontPage(req, res) {
 
-        }
-        else
-        {
-          resultJSON.outcome="losses";
-          resultJSON.losses += 1;
-        }
-      }
-      else if(requestEle==="paper")
-      {
-        if(selectEle ==="rock" || selectEle ==="spock")
-        {
-          resultJSON.outcome="win";
-          resultJSON.wins += 1;
-        }
-        else if(selectEle === "paper")
-        {
-          resultJSON.outcome="ties";
-          resultJSON.ties += 1;
 
-        }
-        else
-        {
-          resultJSON.outcome="losses";
-          resultJSON.losses += 1;
-        }
-      }
-      else if(requestEle==="scissors")
-      {
-        if(selectEle ==="paper" || selectEle ==="lizard")
-        {
-          resultJSON.outcome="win";
-          resultJSON.wins += 1;
-        }
-        else if(selectEle === "scissors")
-        {
-          resultJSON.outcome="ties";
-          resultJSON.ties += 1;
 
-        }
-        else
-        {
-          resultJSON.outcome="losses";
-          resultJSON.losses += 1;
-        }
-      }
-      else if(requestEle==="lizard")
-      {
-        if(selectEle ==="paper" || selectEle === "spock")
-        {
-          resultJSON.outcome="win";
-          resultJSON.wins += 1;
-        }
-        else if(selectEle === "lizard")
-        {
-          resultJSON.outcome="ties";
-          resultJSON.ties += 1;
 
-        }
-        else
-        {
-          resultJSON.outcome="losses";
-          resultJSON.losses += 1;
-        }
-      }
-      else if(requestEle==="spock")
-      {
-        if(selectEle ==="scissors" || selectEle ==="rock")
-        {
-          resultJSON.outcome="win";
-          resultJSON.wins += 1;
-        }
-        else if(selectEle === "spock")
-        {
-          resultJSON.outcome="ties";
-          resultJSON.ties += 1;
+    if (req.method === "POST" && (req.url === "/play/rock" || req.url === "/play/paper" ||
+            req.url === "/play/scissors" || req.url === "/play/lizard" || req.url === "/play/spock")) {
 
-        }
-        else
-        {
-          resultJSON.outcome="losses";
-          resultJSON.losses += 1;
-        }
-      }
-      else
-      {
-        resultJSON={};
-      }
-      return resultJSON;
-    }
-
-    function frontPage(req, res) {
-        res.writeHead(200, {
-            "Content-Type": "application/json"
+        /*var chunk = '';
+        req.on('data', function (data) {
+            chunk += data;
         });
+        req.on('end', function () {
+          requestEle=(querystring.parse(chunk)).element;
+          console.log("request"+requestEle);
+        });*/
 
+        var parts = req.url.split("/");
+            var requestEle = parts[2];
+        resultJSON = gameLogic(requestEle, resultJSON, gameArray);
+        console.log(resultJSON);
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(resultJSON));
+        res.end();
+    } else {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        beginPage(res);
 
-        var requestEle;
-        if (req.method === "POST" && (req.url === "/play/rock" || req.url === "/play/paper" ||
-        req.url === "/play/scissors"  || req.url === "/play/lizard" || req.url === "/play/spock")) {
+        postAction(res, "rock");
+        postAction(res, "paper");
+        postAction(res, "scissors");
+        postAction(res, "lizard");
+        postAction(res, "spock");
 
-              /*var chunk = '';
-              req.on('data', function (data) {
-                  chunk += data;
-              });
-              req.on('end', function () {
-                requestEle=(querystring.parse(chunk)).element;
-                console.log("request"+requestEle);
-              });*/
-              var parts=req.url.split("/"),
-              requestEle=parts[2];
-              resultJSON=gameLogic(res,requestEle,resultJSON,gameArray)
-              res.write(JSON.stringify(resultJSON));
-              res.end();
-        }
-        else
-        {
-          res.writeHead(200, {
-              "Content-Type": "text/html"
-          });
-          beginPage(res);
-
-          postAction(res,"rock");
-          postAction(res,"paper");
-          postAction(res,"scissors");
-          postAction(res,"lizard");
-          postAction(res,"spock");
-
-          endPage(res);
-        }
-
-
+        endPage(res);
     }
-server=http.createServer(frontPage);
+
+
+}
+server = http.createServer(frontPage);
 server.listen(3000);
 
 console.log("Server listening on port 3000 http://localhost:3000");
