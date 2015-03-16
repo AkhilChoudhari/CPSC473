6d50ae1dac3a866fa467fde30dc1c082db8b3c96
nikuldchauhan <dhaval301092@gmail.com>
2015-03-16 01:20:42 -0700

    Worked with all modes

--- assignment_4/server.js
+++ assignment_4/server.js
@@ -33,6 +33,7 @@ var http = require("http"),
     function gameLogic(res,requestEle,resultJSON,gameArray)
     {
       var selectEle=gameArray[Math.floor(Math.random() * gameArray.length)];
+      console.log(selectEle);
       if(requestEle==="rock")
       {
         if(selectEle ==="scissors" || selectEle ==="lizard")
@@ -52,6 +53,86 @@ var http = require("http"),
           resultJSON.losses += 1;
         }
       }
+      else if(requestEle==="paper")
+      {
+        if(selectEle ==="rock" || selectEle ==="spock")
+        {
+          resultJSON.outcome="win";
+          resultJSON.wins += 1;
+        }
+        else if(selectEle === "paper")
+        {
+          resultJSON.outcome="ties";
+          resultJSON.ties += 1;
+
+        }
+        else
+        {
+          resultJSON.outcome="losses";
+          resultJSON.losses += 1;
+        }
+      }
+      else if(requestEle==="scissors")
+      {
+        if(selectEle ==="paper" || selectEle ==="lizard")
+        {
+          resultJSON.outcome="win";
+          resultJSON.wins += 1;
+        }
+        else if(selectEle === "scissors")
+        {
+          resultJSON.outcome="ties";
+          resultJSON.ties += 1;
+
+        }
+        else
+        {
+          resultJSON.outcome="losses";
+          resultJSON.losses += 1;
+        }
+      }
+      else if(requestEle==="lizard")
+      {
+        if(selectEle ==="paper" || selectEle === "spock")
+        {
+          resultJSON.outcome="win";
+          resultJSON.wins += 1;
+        }
+        else if(selectEle === "lizard")
+        {
+          resultJSON.outcome="ties";
+          resultJSON.ties += 1;
+
+        }
+        else
+        {
+          resultJSON.outcome="losses";
+          resultJSON.losses += 1;
+        }
+      }
+      else if(requestEle==="spock")
+      {
+        if(selectEle ==="scissors" || selectEle ==="rock")
+        {
+          resultJSON.outcome="win";
+          resultJSON.wins += 1;
+        }
+        else if(selectEle === "spock")
+        {
+          resultJSON.outcome="ties";
+          resultJSON.ties += 1;
+
+        }
+        else
+        {
+          resultJSON.outcome="losses";
+          resultJSON.losses += 1;
+        }
+      }
+      else
+      {
+        resultJSON={};
+      }
       return resultJSON;
     }
 
@@ -63,7 +144,7 @@ var http = require("http"),
 
         var requestEle;
         if (req.method === "POST" && (req.url === "/play/rock" || req.url === "/play/paper" ||
-        req.url === "/play/scissor"  || req.url === "/play/lizard" || req.url === "/play/spock")) {
+        req.url === "/play/scissors"  || req.url === "/play/lizard" || req.url === "/play/spock")) {
 
               /*var chunk = '';
               req.on('data', function (data) {
@@ -88,7 +169,7 @@ var http = require("http"),
 
           postAction(res,"rock");
           postAction(res,"paper");
-          postAction(res,"scissor");
+          postAction(res,"scissors");
           postAction(res,"lizard");
           postAction(res,"spock");
 
